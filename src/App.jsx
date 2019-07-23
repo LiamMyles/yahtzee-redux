8b38967d525538668d84622fc1990e34a08dd9e1
Liam Myles <liamthemonkey92@gmail.com>
2019-07-23 22:03:16 +0100

    Added basic hold code for dice

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-const initialDiceState = [...Array(6)].map((item, index) => ({
+const initialDiceState = [...Array(5)].map(() => ({
   theme: 3,
   side: 1,
-  isRolling: false
+  isRolling: false,
+  isHeld: false
 }));
 
-function diceReducer(state, { type, diceNumber }) {
+function diceReducer(state, { type, dieNumber }) {
   switch (type) {
-    case "rollSingleDice":
+    case "rollDie":
       return state.map((die, index) => {
-        if (diceNumber === index) {
+        if (dieNumber === index && !die.isHeld) {
           return { ...die, side: rollDice() };
         } else {
           return die;
@@ -24,8 +25,22 @@ function diceReducer(state, { type, diceNumber }) {
       });
     case "rollAllDice":
       return state.map(die => {
-        return { ...die, side: rollDice() };
+        if (die.isHeld) {
+          return { ...die };
+        } else {
+          return { ...die, side: rollDice() };
+        }
       });
+    case "holdDie":
+      return state.map((die, index) => {
+        if (dieNumber === index) {
+          return { ...die, isHeld: true };
+        } else {
+          return die;
+        }
+      });
+    case "resetHeld":
+      return state.map(die => ({ ...die, isHeld: false }));
     default:
       throw new Error();
   }
@@ -33,33 +48,65 @@ function diceReducer(state, { type, diceNumber }) {
 
 function App() {
   const [diceState, dispatch] = useReducer(diceReducer, initialDiceState);
-  console.log(diceState);
   return (
     <div>
-      <section
+      <section>
+        {diceState.map((dieState, index) => (
+          <div key={index}>
+            <Die
+              side={dieState.side}
+              theme={dieState.theme}
+              isHeld={dieState.isHeld}
+              clickFunction={() => {
+                dispatch({ type: "rollDie", dieNumber: index });
+              }}
+            />
+
+            <button
+              onClick={() => {
+                dispatch({ type: "holdDie", dieNumber: index });
+              }}
+            >
+              Hold Die
+            </button>
+          </div>
+        ))}
+      </section>
+      <button
         onClick={() => {
           dispatch({ type: "rollAllDice" });
         }}
       >
-        {diceState.map((dieState, index) => (
-          <Die side={dieState.side} theme={dieState.theme} key={index} />
-        ))}
-        <Die />
-      </section>
+        Roll All
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "resetHeld" });
+        }}
+      >
+        Reset Held
+      </button>
     </div>
   );
 }
 
-function Die({ side = 1, theme = 1 }) {
+function Die({ side = 1, theme = 1, clickFunction, isHeld }) {
   return (
-    <ul className={`die die--theme-${theme}`} data-current-side={side}>
-      <li className="pip" />
-      <li className="pip" />
-      <li className="pip" />
-      <li className="pip" />
-      <li className="pip" />
-      <li className="pip" />
-    </ul>
+    <>
+      {isHeld && <h1>Holding</h1>}
+      <ul
+        className={`die die--theme-${theme}`}
+        data-current-side={side}
+        onClick={clickFunction}
+      >
+        <li className="pip" />
+        <li className="pip" />
+        <li className="pip" />
+        <li className="pip" />
+        <li className="pip" />
+        <li className="pip" />
+      </ul>
+    </>
   );
 }
 
