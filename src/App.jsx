3d6c691c133f5719a88973db0a0b0abde1412b60
Liam Myles <liamthemonkey92@gmail.com>
2019-08-04 00:41:58 +0100

    Add scoreboard Logic stuff

--- src/App.jsx
+++ src/App.jsx
@@ -45,12 +45,89 @@ function diceReducer(state, { type, dieNumber }) {
       throw new Error();
   }
 }
+const initialScoreState = {
+  ones: {
+    score: 0,
+    isScored: false
+  },
+  twos: {
+    score: 0,
+    isScored: false
+  },
+  threes: {
+    score: 0,
+    isScored: false
+  },
+  fours: {
+    score: 0,
+    isScored: false
+  },
+  fives: {
+    score: 0,
+    isScored: false
+  },
+  sixes: {
+    score: 0,
+    isScored: false
+  },
+  threeKinds: {
+    score: 0,
+    isScored: false
+  },
+  fourKinds: {
+    score: 0,
+    isScored: false
+  },
+  smallStraight: {
+    score: 0,
+    isScored: false
+  },
+  longStraight: {
+    score: 0,
+    isScored: false
+  },
+  chance: {
+    score: 0,
+    isScored: false
+  },
+  yahtzee: {
+    score: 0,
+    isScored: false
+  }
+};
+function scoreReducer(state, { type, alias, score }) {
+  switch (type) {
+    case "addScore": {
+      return { ...state, [alias]: { ...state[alias], score, isScored: true } };
+    }
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
-  const [diceState, dispatch] = useReducer(diceReducer, initialDiceState);
+  // useEvent? Thing will take scoreState and DiceState to trigger updates to a GameState reducer
+
+  const [diceState, dispatchDice] = useReducer(diceReducer, initialDiceState);
+  const [scoreState, dispatchScores] = useReducer(
+    scoreReducer,
+    initialScoreState
+  );
+
+  let totalScores = [];
+  for (const score in scoreState) {
+    if (scoreState.hasOwnProperty(score)) {
+      totalScores.push(scoreState[score].score);
+    }
+  }
   return (
     <>
-      <ScoreBoard dice={diceState.map(({ side }) => side)} />
+      <h1>Total Score {totalScores.reduce((a, b) => a + b, 0)}</h1>
+      <ScoreBoard
+        dice={diceState.map(({ side }) => side)}
+        scoreState={scoreState}
+        dispatchScores={dispatchScores}
+      />
       <div>
         <section className="dice">
           {diceState.map((dieState, index) => (
@@ -60,13 +137,13 @@ function App() {
                 theme={dieState.theme}
                 isHeld={dieState.isHeld}
                 clickFunction={() => {
-                  dispatch({ type: "rollDie", dieNumber: index });
+                  dispatchDice({ type: "rollDie", dieNumber: index });
                 }}
               />
 
               <button
                 onClick={() => {
-                  dispatch({ type: "holdDie", dieNumber: index });
+                  dispatchDice({ type: "holdDie", dieNumber: index });
                 }}
               >
                 Hold Die
@@ -76,14 +153,14 @@ function App() {
         </section>
         <button
           onClick={() => {
-            dispatch({ type: "rollAllDice" });
+            dispatchDice({ type: "rollAllDice" });
           }}
         >
           Roll All
         </button>
         <button
           onClick={() => {
-            dispatch({ type: "resetHeld" });
+            dispatchDice({ type: "resetHeld" });
           }}
         >
           Reset Held
@@ -113,212 +190,263 @@ function Die({ side = 1, theme = 1, clickFunction, isHeld }) {
   );
 }
 
-function ScoreButtonSelection({ isValidScore, children }) {
-  return <button disabled={!isValidScore}>{children}</button>;
+function ScoreButtonSelection({
+  name,
+  isValidScore,
+  currentScore,
+  topScore,
+  isScored,
+  dispatchScores,
+  alias,
+  scoredScore
+}) {
+  const showCurrentScore =
+    !isScored &&
+    currentScore !== 0 &&
+    currentScore !== undefined &&
+    isValidScore;
+  return (
+    <button
+      onClick={() =>
+        dispatchScores({ type: "addScore", alias, score: currentScore })
+      }
+      disabled={!isValidScore || isScored}
+      style={isScored ? { background: "red" } : {}}
+    >
+      {`${name} max: ${topScore} `}
+      {showCurrentScore && `Current: ${currentScore}`}
+      {isScored && `Scored: ${scoredScore}`}
+    </button>
+  );
 }
+function getUpperScoreFunction(scoringNumber) {
+  return dice =>
+    dice
+      .filter(die => die === scoringNumber)
+      .reduce((prev, current) => prev + current, 0);
+}
+const scoreboardLogic = {
+  upper: [
+    {
+      test: dice => dice.includes(1),
+      name: "ones",
+      alias: "ones",
+      topScore: 5,
+      getScore: getUpperScoreFunction(1)
+    },
+    {
+      test: dice => dice.includes(2),
+      name: "twos",
+      alias: "twos",
+      topScore: 10,
+      getScore: getUpperScoreFunction(2)
+    },
+    {
+      test: dice => dice.includes(3),
+      name: "threes",
+      alias: "threes",
+      topScore: 15,
+      getScore: getUpperScoreFunction(3)
+    },
+    {
+      test: dice => dice.includes(4),
+      name: "fours",
+      alias: "fours",
+      topScore: 20,
+      getScore: getUpperScoreFunction(4)
+    },
+    {
+      test: dice => dice.includes(5),
+      name: "fives",
+      alias: "fives",
+      topScore: 25,
+      getScore: getUpperScoreFunction(5)
+    },
+    {
+      test: dice => dice.includes(6),
+      name: "sixes",
+      alias: "sixes",
+      topScore: 30,
+      getScore: getUpperScoreFunction(6)
+    }
+  ],
+  lower: [
+    {
+      test: dice => {
+        const newDice = [...dice];
+        newDice.sort();
 
-function ScoreBoard({ dice }) {
-  function getUpperScoreFunction(scoringNumber) {
-    return dice =>
-      dice
-        .filter(die => die === scoringNumber)
-        .reduce((prev, current) => prev + current, 0);
-  }
+        const firstHalf = newDice.slice(0, 3);
+        const endHalf = newDice.slice(2);
 
-  const scores = {
-    upper: [
-      {
-        test: dice => dice.includes(1),
-        name: "ones",
-        topScore: 5,
-        getScore: getUpperScoreFunction(1)
+        return (
+          firstHalf.every(die => die === firstHalf[0]) ||
+          endHalf.every(die => die === endHalf[0])
+        );
       },
-      {
-        test: dice => dice.includes(2),
-        name: "twos",
-        topScore: 10,
-        getScore: getUpperScoreFunction(2)
-      },
-      {
-        test: dice => dice.includes(3),
-        name: "threes",
-        topScore: 15,
-        getScore: getUpperScoreFunction(3)
-      },
-      {
-        test: dice => dice.includes(4),
-        name: "fours",
-        topScore: 20,
-        getScore: getUpperScoreFunction(4)
-      },
-      {
-        test: dice => dice.includes(5),
-        name: "fives",
-        topScore: 25,
-        getScore: getUpperScoreFunction(5)
-      },
-      {
-        test: dice => dice.includes(6),
-        name: "sixes",
-        topScore: 30,
-        getScore: getUpperScoreFunction(6)
+      name: "threeKinds",
+      alias: "threeKinds",
+      topScore: 18,
+      getScore: dice => {
+        const newDice = [...dice];
+        newDice.sort();
+
+        const firstHalf = newDice.slice(0, 3);
+        const endHalf = newDice.slice(2);
+
+        if (firstHalf.every(die => die === firstHalf[0]))
+          return firstHalf[0] * 3;
+        if (endHalf.every(die => die === endHalf[0])) return endHalf[0] * 3;
       }
-    ],
-    lower: [
-      {
-        test: dice => {
-          const newDice = [...dice];
-          newDice.sort();
+    },
+    {
+      test: dice => {
+        const newDice = [...dice];
+        newDice.sort();
 
-          const firstHalf = newDice.slice(0, 3);
-          const endHalf = newDice.slice(2);
+        const firstHalf = newDice.slice(0, 4);
+        const endHalf = newDice.slice(1);
 
-          return (
-            firstHalf.every(die => die === firstHalf[0]) ||
-            endHalf.every(die => die === endHalf[0])
-          );
-        },
-        name: "threeKinds",
-        topScore: 18,
-        getScore: dice => {
-          const newDice = [...dice];
-          newDice.sort();
-
-          const firstHalf = newDice.slice(0, 3);
-          const endHalf = newDice.slice(2);
-
-          if (firstHalf.every(die => die === firstHalf[0]))
-            return firstHalf[0] * 3;
-          if (endHalf.every(die => die === endHalf[0])) return endHalf[0] * 3;
-        }
+        return (
+          firstHalf.every(die => die === firstHalf[0]) ||
+          endHalf.every(die => die === endHalf[0])
+        );
       },
-      {
-        test: dice => {
-          const newDice = [...dice];
-          newDice.sort();
+      name: "fourKinds",
+      alias: "fourKinds",
+      topScore: 24,
+      getScore: dice => {
+        const newDice = [...dice];
+        newDice.sort();
 
-          const firstHalf = newDice.slice(0, 4);
-          const endHalf = newDice.slice(1);
+        const firstHalf = newDice.slice(0, 4);
+        const endHalf = newDice.slice(1);
 
-          return (
-            firstHalf.every(die => die === firstHalf[0]) ||
-            endHalf.every(die => die === endHalf[0])
-          );
-        },
-        name: "fourKinds",
-        topScore: 24,
-        getScore: dice => {
-          const newDice = [...dice];
-          newDice.sort();
-
-          const firstHalf = newDice.slice(0, 4);
-          const endHalf = newDice.slice(1);
-
-          if (firstHalf.every(die => die === firstHalf[0]))
-            return firstHalf[0] * 4;
-          if (endHalf.every(die => die === endHalf[0])) return endHalf[0] * 4;
-        }
-      },
-      {
-        test: dice => {
-          const newDice = [...dice];
-          newDice.sort();
-
-          const firstHalf = newDice.slice(0, 4);
-          const endHalf = newDice.slice(2);
-          const checkAllAreConsecutive = (die, index, dice) => {
-            if (index === dice.length - 1) return true;
-            return die + 1 === dice[index + 1];
-          };
-          return (
-            firstHalf.every(checkAllAreConsecutive) ||
-            endHalf.every(checkAllAreConsecutive)
-          );
-        },
-        name: "smallStraight",
-        topScore: 30,
-        getScore: dice => {
-          const newDice = [...dice];
-          newDice.sort();
-
-          const firstHalf = newDice.slice(0, 4);
-          const endHalf = newDice.slice(2);
-          const checkAllAreConsecutive = (die, index, dice) => {
-            if (index === dice.length - 1) return true;
-            return die + 1 === dice[index + 1];
-          };
-          if (
-            firstHalf.every(checkAllAreConsecutive) ||
-            endHalf.every(checkAllAreConsecutive)
-          ) {
-            return 30;
-          } else {
-            return 0;
-          }
-        }
-      },
-      {
-        test: dice => {
-          const newDice = [...dice];
-          newDice.sort();
-
-          const checkAllAreConsecutive = (die, index, dice) => {
-            if (index === dice.length - 1) return true;
-            return die + 1 === dice[index + 1];
-          };
-          return newDice.every(checkAllAreConsecutive);
-        },
-        name: "longStraight",
-        topScore: 40,
-        getScore: dice => {
-          const newDice = [...dice];
-          newDice.sort();
-
-          const checkAllAreConsecutive = (die, index, dice) => {
-            if (index === dice.length - 1) return true;
-            return die + 1 === dice[index + 1];
-          };
-          if (newDice.every(checkAllAreConsecutive)) {
-            return 40;
-          } else {
-            return 0;
-          }
-        }
-      },
-      {
-        test: () => true,
-        name: "chance",
-        topScore: 30,
-        getScore: dice => dice.reduce((prev, current) => prev + current, 0)
-      },
-      {
-        test: dice => new Set(dice).size === 1,
-        name: "yahtzee",
-        topScore: 50,
-        getScore: dice => 50
+        if (firstHalf.every(die => die === firstHalf[0]))
+          return firstHalf[0] * 4;
+        if (endHalf.every(die => die === endHalf[0])) return endHalf[0] * 4;
       }
-    ]
-  };
+    },
+    {
+      test: dice => {
+        const newDice = [...dice];
+        newDice.sort();
 
+        const firstHalf = newDice.slice(0, 4);
+        const endHalf = newDice.slice(2);
+        const checkAllAreConsecutive = (die, index, dice) => {
+          if (index === dice.length - 1) return true;
+          return die + 1 === dice[index + 1];
+        };
+        return (
+          firstHalf.every(checkAllAreConsecutive) ||
+          endHalf.every(checkAllAreConsecutive)
+        );
+      },
+      name: "smallStraight",
+      alias: "smallStraight",
+      topScore: 30,
+      getScore: dice => {
+        const newDice = [...dice];
+        newDice.sort();
+
+        const firstHalf = newDice.slice(0, 4);
+        const endHalf = newDice.slice(2);
+        const checkAllAreConsecutive = (die, index, dice) => {
+          if (index === dice.length - 1) return true;
+          return die + 1 === dice[index + 1];
+        };
+        if (
+          firstHalf.every(checkAllAreConsecutive) ||
+          endHalf.every(checkAllAreConsecutive)
+        ) {
+          return 30;
+        } else {
+          return 0;
+        }
+      }
+    },
+    {
+      test: dice => {
+        const newDice = [...dice];
+        newDice.sort();
+
+        const checkAllAreConsecutive = (die, index, dice) => {
+          if (index === dice.length - 1) return true;
+          return die + 1 === dice[index + 1];
+        };
+        return newDice.every(checkAllAreConsecutive);
+      },
+      name: "longStraight",
+      alias: "longStraight",
+      topScore: 40,
+      getScore: dice => {
+        const newDice = [...dice];
+        newDice.sort();
+
+        const checkAllAreConsecutive = (die, index, dice) => {
+          if (index === dice.length - 1) return true;
+          return die + 1 === dice[index + 1];
+        };
+        if (newDice.every(checkAllAreConsecutive)) {
+          return 40;
+        } else {
+          return 0;
+        }
+      }
+    },
+    {
+      test: () => true,
+      name: "chance",
+      alias: "chance",
+      topScore: 30,
+      getScore: dice => dice.reduce((prev, current) => prev + current, 0)
+    },
+    {
+      test: dice => new Set(dice).size === 1,
+      name: "yahtzee",
+      alias: "yahtzee",
+      topScore: 50,
+      getScore: () => 50
+    }
+  ]
+};
+function ScoreBoard({ dice, scoreState, dispatchScores }) {
   return (
     <section className="score-board">
       <ul className="score-board__scores score-board__scores--upper-section">
-        {scores.upper.map(({ name, test, topScore, getScore }) => {
-          return (
-            <ScoreButtonSelection isValidScore={test(dice)} key={name}>
-              {`${name} ${topScore} ${getScore(dice)}`}
-            </ScoreButtonSelection>
-          );
-        })}
+        {scoreboardLogic.upper.map(
+          ({ name, test, topScore, getScore, alias }) => (
+            <ScoreButtonSelection
+              currentScore={getScore(dice)}
+              isValidScore={test(dice)}
+              topScore={topScore}
+              name={name}
+              isScored={scoreState[alias].isScored}
+              scoredScore={scoreState[alias].score}
+              dispatchScores={dispatchScores}
+              alias={alias}
+              key={alias}
+            />
+          )
+        )}
       </ul>
       <ul className="score-board__scores score-board__scores--lower-section">
-        {scores.lower.map(({ name, test, topScore, getScore }) => {
-          return (
-            <ScoreButtonSelection isValidScore={test(dice)} key={name}>
-              {`${name} ${topScore} ${getScore(dice)}`}
-            </ScoreButtonSelection>
-          );
-        })}
+        {scoreboardLogic.lower.map(
+          ({ name, test, topScore, getScore, alias }) => (
+            <ScoreButtonSelection
+              currentScore={getScore(dice)}
+              isValidScore={test(dice)}
+              topScore={topScore}
+              key={alias}
+              isScored={scoreState[alias].isScored}
+              scoredScore={scoreState[alias].score}
+              dispatchScores={dispatchScores}
+              alias={alias}
+              name={name}
+            />
+          )
+        )}
       </ul>
     </section>
   );
