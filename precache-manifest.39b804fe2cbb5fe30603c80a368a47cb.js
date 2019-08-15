bb59af0d5df7d89cba014567d052bd59d9b65855
Liam Myles <liamthemonkey92@gmail.com>
2019-08-15 22:18:25 +0100

    Updates

---
+++ precache-manifest.39b804fe2cbb5fe30603c80a368a47cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c144ac228d50a5f059da814228d84e11",
+    "url": "/yahtzee-redux/index.html"
+  },
+  {
+    "revision": "2e9ad22364d7ce1f964e",
+    "url": "/yahtzee-redux/static/css/main.9b886751.chunk.css"
+  },
+  {
+    "revision": "648f0c46efd4258b1717",
+    "url": "/yahtzee-redux/static/js/2.7518c56c.chunk.js"
+  },
+  {
+    "revision": "2e9ad22364d7ce1f964e",
+    "url": "/yahtzee-redux/static/js/main.c04ba917.chunk.js"
+  },
+  {
+    "revision": "ec35b548dd025721bc1f",
+    "url": "/yahtzee-redux/static/js/runtime~main.458ccfaa.js"
+  }
+]);
